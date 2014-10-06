07019258331d18f83347aa1b88df587b296b0cbb
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-10-06 17:23:19 +0200

    ejercicio3 prac3 terminado forma reducida

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -2,92 +2,33 @@
 # -*- coding: iso-8859-15 -*-
 # Alba Oterino Corral
 
+
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
-    
-        self.width = ""
-        self.height = ""
-        self.backgroundcolor = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.src = ""
-        self.region = ""
-        self.begin = ""
-        self.dur = ""        
+    def __init__(self):
         self.etiquetas = []
-   
+        self.tags = ["root-layout", "region", "img", "audio", "textstream"]
+        self.atributos = {
+            "root-layout": ["width", "height", "background-color"],
+            "region": ["id", "top", "botton", "left", "right"],
+            "img": ["src", "region", "begin", "dur"],
+            "audio": ["src", "begin", "dur"],
+            "textstream": ["src", "region"]
+        }
+
     def startElement(self, name, attrs):
 
-     if name == 'root-layout':
-        rootlayout = {}
-        self.width = attrs.get('width',"")
-        rootlayout['width'] = self.width
-        self.height = attrs.get('height',"")
-        rootlayout['height'] = self.height
-        self.backgroundcolor = attrs.get('background-color',"")
-        rootlayout['background-color'] = self.backgroundcolor
-        print "diccionario rootlayout"
-        print rootlayout
-        self.etiquetas.append(rootlayout)
+        diccionario = {}
+        if name in self.tags:
+            diccionario['etiqueta'] = name
+            for atributo in self.atributos[name]:
+                diccionario[atributo] = attrs.get(atributo, "")
+            self.etiquetas.append(diccionario)
 
-     elif name == 'region':
-        region = {}
-        self.id = attrs.get('id',"")
-        region['id'] = self.id       
-        self.top = attrs.get('top',"")
-        region['top'] = self.top
-        self.bottom = attrs.get('bottom',"")
-        self.left = attrs.get('left',"")
-        region['left'] = self.left
-        self.right = attrs.get('right',"")
-        region['right'] = self.right
-        print "diccionario region"
-        print region
-        self.etiquetas.append(region)
-
-     elif name == 'img':
-        img = {}
-        self.src = attrs.get('src',"")
-        img['src'] = self.src
-        self.region = attrs.get('region',"")
-        img['region'] = self.region
-        self.begin = attrs.get('begin',"")
-        img['begin'] = self.region
-        self.dur = attrs.get('dur',"")
-        img['dur'] = self.dur
-        print "diccionario imagen"
-        print img
-        self.etiquetas.append(img)    
-     elif name == 'audio':
-        audio = {}
-        self.src = attrs.get('src',"")
-        audio['src'] = self.src
-        self.begin = attrs.get('begin',"")
-        audio['begin'] = self.begin
-        self.dur = attrs.get('dur',"")
-        audio['dur'] = self.dur     
-        print "diccionario audio"
-        print audio
-        self.etiquetas.append(audio)
-
-     elif name == 'textstream':
-        textstream = {}
-        self.src = attrs.get('src',"")
-        textstream['src'] = self.src
-        self.region = attrs.get('region',"")
-        textstream['region'] = self.region
-        print "diccionario textstream"
-        print textstream
-        self.etiquetas.append(textstream)
-
-        
     def get_tags(self):
         return self.etiquetas
 
@@ -96,8 +37,7 @@ if __name__ == "__main__":
     Programa principal
     """
     parser = make_parser()
-    cHandler = SmallSMILHandler() #objeto
-    parser.setContentHandler(cHandler)   
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
     print cHandler.get_tags()
-    
